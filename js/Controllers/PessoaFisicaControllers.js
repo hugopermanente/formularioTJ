bf83ddb6f313ef93e04056654d89f4bfa0dba7ee
Administrator <administrator@IDIOMAS.LOCAL>
2019-03-20 16:16:50 -0300

    Movels

--- js/Controllers/PessoaFisicaControllers.js
+++ js/Controllers/PessoaFisicaControllers.js
@@ -1,6 +1,15 @@
-class PessoaFiscaControllers {
+class PessoaFisicaControllers {
     constructor( ) {
+        let $ = document.querySelector.bind(document);
         
+        this.inputNome = $("#inputName");
+        this.inputEmail = $("#inputEmail");
+        this.inputTel = $("#inputTel");
     }
+    
+     adiciona(event) {
+        event.preventDefault( );
 
+        alert(this.inputNome.value);
+     }
 }
